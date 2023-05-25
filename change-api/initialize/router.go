5225f7fb93e7b48dff3727c89885d5409b430c2f
administrator <admin@qq.com>
2023-05-25 23:15:00 +0800

    跨域，请求日志中间件

--- change-api/initialize/router.go
+++ change-api/initialize/router.go
@@ -13,6 +13,8 @@ func Router() *gin.Engine {
 	r := gin.New()
 
 	// 中间件
+	r.Use(middleware.AccessLog) // 访问日志中间件，用于打印请求日志
+	r.Use(middleware.Cors)      // 异常访问中间件，用于允许跨域访问
 	r.Use(middleware.Exception) // 异常捕获中间件，用于处理用户响应
 
 	// 基础路由组
