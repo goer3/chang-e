2ac60c795f860948b91efb9fcf30ba375743709c
administrator <admin@qq.com>
2023-06-02 00:43:01 +0800

    API 文档

--- change-api/api/index.go
+++ change-api/api/index.go
@@ -12,12 +12,12 @@ import (
 	"github.com/golang-module/carbon/v2"
 )
 
-// @Summary		ping
-// @Description	ping 测试处理函数
-// @Tags			Base
-// @Success		200	{object}	response.Response	"请求成功"
-// @Failure		200	{object}	response.Response	"请求错误"
-// @Router			/ping [get]
+//	@Summary		ping
+//	@Description	ping 测试处理函数
+//	@Tags			Base
+//	@Success		200	{object}	response.Response	"请求成功"
+//	@Failure		200	{object}	response.Response	"请求错误"
+//	@Router			/ping [get]
 func PingHandler(ctx *gin.Context) {
 	response.SuccessWithData(map[string]interface{}{
 		"message": "pong",
