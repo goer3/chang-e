4d62c583d61afeaca14abef87c2fe20a2e73f86c
goer3 <1214966109@qq.com>
2023-06-15 21:41:13 +0800

    list 拆分

--- change-web/src/pages/system/users/users-list.jsx
+++ change-web/src/pages/system/users/users-list.jsx
@@ -1,38 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Space, Descriptions, Col, Row, Button, Dropdown, Avatar, Badge, Tag } from 'antd';
-import { DownloadOutlined, DownOutlined, UploadOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Table, Space, Descriptions, Avatar, Badge, Tag } from 'antd';
 import { UserListAPI } from '../../../service';
-
-//////////////////////////////////////////////////////////////////
-// 用户批量操作
-//////////////////////////////////////////////////////////////////
-const UserMultiHandleItems = [
-  {
-    label: '锁定选中用户',
-    key: '1',
-  },
-  {
-    label: '解锁选中用户',
-    key: '2',
-  },
-  {
-    label: '激活选中用户',
-    key: '3',
-  },
-  {
-    label: '删除选中用户',
-    key: '4',
-  },
-];
-
-const UserMultiHandle = (e) => {
-  console.log('数据：', e);
-};
-
-const UserMultiHandleProps = {
-  items: UserMultiHandleItems,
-  onClick: UserMultiHandle,
-};
+import { UserManagementBtnGroup } from './users-list-handle';
 
 //////////////////////////////////////////////////////////////////
 // 表格列定义
@@ -155,30 +124,8 @@ export const UserManagementList = () => {
   return (
     <>
       {/* 按钮组 */}
-      <Row style={{ marginBottom: '10px' }}>
-        <Col span={12}>
-          <Button type="primary" icon={<UserAddOutlined />} className="admin-ant-btn">
-            新建用户
-          </Button>
-          <Button icon={<UploadOutlined />} className="admin-ant-btn">
-            导入用户
-          </Button>
-        </Col>
-        <Col span={12} className="align-right">
-          <Button icon={<DownloadOutlined />} className="admin-ant-btn">
-            下载模板
-          </Button>
+      <UserManagementBtnGroup />
 
-          <Dropdown menu={UserMultiHandleProps}>
-            <Button danger className="admin-ant-btn admin-ant-btn-last">
-              <Space>
-                <DownOutlined />
-                批量操作
-              </Space>
-            </Button>
-          </Dropdown>
-        </Col>
-      </Row>
       {/*数据*/}
       {isLoading ? (
         <div>正在加载中...</div>
